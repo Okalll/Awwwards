8466bbc00a1da9e0d5b5a89afad0e9fa4fe3c184
Okall Vivian <vivieokall@gmail.com>
2019-03-18 12:25:56 +0300

    Add profile model

--- awwward/models.py
+++ awwward/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
+from datetime import datetime
+from django.utils import timezone
+from django.db.models.signals import post_save
 
 # Create your models here.
+class Profile(models.Model):
+    pro_photo = models.ImageField(upload_to='images/', null=True)
+    bio = models.TextField(null=True)
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, related_name='profile')
+
+    def __str__(self):
+        return self.user.username
+
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()
