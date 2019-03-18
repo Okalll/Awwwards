b6d0b44c62036a1a98355c075cfd9eeeaa92e02f
Okall Vivian <vivieokall@gmail.com>
2019-03-18 23:15:53 +0300

    Modify file

--- awwward/views.py
+++ awwward/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render, redirect
 from django.http  import HttpResponse
-from .forms import SignupForm, ProfileForm
+from .forms import SignupForm, ProfileForm, ProjectForm
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.models import User
+from django.urls import reverse
+from .models import *
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required(login_url='/accounts/login/')
 def home(request):
 
    return render(request, 'home.html')
@@ -18,16 +22,16 @@ def signup(request):
             user.is_active = True
             user.save()
 
-            return render(request, 'profile.html')
+            return render(request, 'registration/login.html/')
     else:
         form = SignupForm()
     return render(request, 'signup.html', {'form': form})
 
 def profile(request):
-    images = Image.objects.all()
+    images = Profile.objects.all()
     current_user = request.user
-    profile = Profile.objects.filter(user=current_user)
-    posts = Image.objects.filter(user=current_user)
+    profile = Profile.objects.all()
+    posts = Profile.objects.all()
     image_form = ProfileForm()
     if request.method == 'POST':
         image_form = ProfileForm(
@@ -37,5 +41,44 @@ def profile(request):
         else:
             image_form = ProfileForm()
 
-    return render(request, 'profile.html', {"image_form": image_form, "posts": posts, "profile": profile, "images": images})
+    return render(request, 'profile.html', {"image_form": image_form, "profile": profile, "images": images})
 
+def upload(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = ProjectForm(request.POST, request.FILES)
+        if form.is_valid():
+            projects = form.save(commit=False)
+            projects.poster = current_user
+            projects.save()
+        return redirect('home')
+
+    else:
+        form = ProjectForm()
+    return render(request, 'upload.html', {"form": form })
+
+@login_required(login_url='/accounts/login/')
+def project(request,project_id):
+   projects = Projects.objects.get(id=project_id)
+   rating = round(((project.design + project.usability + project.content)/3),2)
+   if request.method == 'POST':
+       form = VoteForm(request.POST)
+       if form.is_valid:
+           project.vote_submissions += 1
+           if project.design == 0:
+               project.design = int(request.POST['design'])
+           else:
+               project.design = (project.design + int(request.POST['design']))/2
+           if project.usability == 0:
+               project.usability = int(request.POST['usability'])
+           else:
+               project.usability = (project.design + int(request.POST['usability']))/2
+           if project.content == 0:
+               project.content = int(request.POST['content'])
+           else:
+               project.content = (project.design + int(request.POST['content']))/2
+           project.save()
+           return redirect(reverse('project',args=[project.id]))
+   else:
+       form_ = VoteForm()
+   return render(request,'project.html',{'form_':form_,'projects':projects,'rating':rating})
