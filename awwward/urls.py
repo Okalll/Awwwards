9f4e4eb05101eac0f962c1492f67adc492507720
Okall Vivian <vivieokall@gmail.com>
2019-03-18 23:16:24 +0300

    Add a link url

--- awwward/urls.py
+++ awwward/urls.py
@@ -1,7 +1,16 @@
 from django.conf.urls import url
 from . import views
+from .views import *
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns=[
-    url('^$',views.home,name = 'home'),
-    url(r'signup', views.signup, name='signup'),
+    url(r'^$', views.signup, name='signup'),
+    url(r'^home/$', views.home, name='home'),
+    url(r'^upload/', views.upload, name='upload'),
+    url(r'^accounts/profile/', views.profile, name='profile'),
+    url(r'^project/', views.project, name='project'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
