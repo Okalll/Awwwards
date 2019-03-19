fe94c85faf4ac2e25b9fc72869ee4b4ced0fdcc0
Okall Vivian <vivieokall@gmail.com>
2019-03-19 09:37:31 +0300

    "deployment"

--- awwward/urls.py
+++ awwward/urls.py
@@ -6,7 +6,7 @@ from django.conf import settings
 
 urlpatterns=[
     url(r'^$', views.signup, name='signup'),
-    url(r'^home/$', views.home, name='home'),
+    url(r'^home/', views.home, name='home'),
     url(r'^upload/', views.upload, name='upload'),
     url(r'^accounts/profile/', views.profile, name='profile'),
     url(r'^project/', views.project, name='project'),
